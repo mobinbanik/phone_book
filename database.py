09d221318d3de9354c4f41766f475321b9557793
Raika <raika@Raik.TT>
2024-03-08 20:00:34 +0330

    add database.py, database_manager.py, requirements.txt, sample_setting.py, init_data.txt to repository. add class Contact, initialize_database(), get_contacts() to database.py. add first_init to sample_setting.py to manage first initialize. update requirements.txt

---
+++ database.py
@@ -0,0 +1,74 @@
+"""Q.
+"""
+import peewee
+import random
+from database_manager import DatabaseManager
+import local_settings
+
+database_manager = DatabaseManager(
+    database_name=local_settings.DATABASE['name'],
+    user=local_settings.DATABASE['user'],
+    password=local_settings.DATABASE['password'],
+    host=local_settings.DATABASE['host'],
+    port=local_settings.DATABASE['port'],
+)
+
+
+class Contact(peewee.Model):
+    first_name = peewee.CharField(
+        max_length=255,
+        null=False,
+        verbose_name='First Name'
+    )
+    last_name = peewee.CharField(
+        max_length=255,
+        null=False,
+        verbose_name='Last Name',
+    )
+    number = peewee.CharField(
+        max_length=20,
+        null=False,
+        verbose_name='Number',
+    )
+    address = peewee.TextField(null=True, verbose_name='Address')
+
+    class Meta:
+        database = database_manager.db
+
+
+def initialize_database():
+    try:
+        database_manager.create_tables(models=[Contact])
+        with open("init_data.txt", "r") as init:
+            for line in init:
+                contact = line.split(",")
+                Contact.create(
+                    first_name=contact[0],
+                    last_name=contact[1],
+                    number=contact[2],
+                    address=contact[3],
+                )
+    except Exception as e:
+        print("Error", e)
+    finally:
+        # closing database connection.
+        if database_manager.db:
+            database_manager.db.close()
+            print("Database connection is closed")
+
+
+def get_contacts():
+    contacts = Contact.select()
+    for contact in contacts:
+        yield {
+            "First Name": contact.first_name,
+            "Last Name": contact.last_name,
+            "Number": contact.number,
+            "Address": contact.address,
+        }
+
+
+# initialize database
+# # Attention: after first start set local_settings.first_init to False
+if local_settings.first_init:
+    initialize_database()
